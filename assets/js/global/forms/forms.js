1a4c47d6ec8d2509e851487ef78b63b21fff83e1
mblais29 <mblais29@gmail.com>
2016-11-06 23:55:59 +0000

    - added functionality to preview the form by getting the form fields from the mongo db and populating a form template dynamically

--- assets/js/global/forms/forms.js
+++ assets/js/global/forms/forms.js
@@ -6,6 +6,8 @@ $(window).on('load',function(){
 	
 	$('#formPreviewClose').on('click', function(){
 		$('#form-preview').slideUp();
+		//Removes all children elements within form
+		$("#formPreview").empty();
 	});
 	
 	$('#formAdd').on('click', function(){
@@ -64,8 +66,8 @@ function insertFormData(formid){
 	$('#form-preview').show();
 	$.ajax('/forms/populate?formid=' + formid,{
       success: function(data) {
-      	console.log(data);
-      	
+      	//console.log(data);
+      	generateForm(data);
       },
       done: function(data){
       	
@@ -74,5 +76,32 @@ function insertFormData(formid){
          console.log(err);
       }
     });
+}
+
+function generateForm(data){
+	for (var i = 0; i < data.length; i++){
+		var obj = data[i];
+	    for (var key in obj){
+	    	//console.log('Key: ' + key);
+	    	//console.log('Key Value: ' + obj[key]);
+	    	if(key === 'formfields'){
+	    		for (var i = 0; i < obj[key].length; i++){
+	    			var formfieldObject = obj[key][i];
+	    			console.log(formfieldObject);
+	    			console.log(formfieldObject.formfieldname);
+	    			
+	    			$('#formPreview').append('<div class="form-group"><label class="control-label col-sm-2" for="'+ formfieldObject.formfieldname + '">' + formfieldObject.formfieldname + ':</label><div class="col-sm-10"><input type="text" class="form-control" id="' + formfieldObject.formfieldname + '" name="' + formfieldObject.formfieldname + '" placeholder="Enter form name..."></div></div>');
+		    		//for (var formfieldKey in formfieldObject){
+		    			//console.log('Key: ' + formfieldKey);
+		    			//console.log('Key Value: ' + formfieldObject[formfieldKey]);
+		    			//console.log(formfieldObject.formfieldname);
+		    			//$('#formPreview').append('<div class="form-group"><label class="control-label col-sm-2" for="'+ formfieldObject.formfieldname + '">' + formfieldObject.formfieldname + '</label><div class="col-sm-10"><input type="text" class="form-control" id="' + formfieldObject.formfieldname + '" name="' + formfieldObject.formfieldname + '" placeholder="Enter form name..."></div></div>');
+
+		    		//}
+	    		}
+	    	}
+	    	
+	    }
+	}
 }
 
